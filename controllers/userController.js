6a85b26cb62ec28c4f40a0c9e8433634923690d9
cristian mejia <comejia@innovacion.gob.sv>
2024-08-07 15:47:14 -0600

    correcciones en parametros que se reciben en un metodo get

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const { User, Message } = require('../models');
+const { Op } = require('sequelize'); // Importar operadores de Sequelize
+const message = require('../models/message');
 
 exports.getProfile = async (req, res) => {
   try {
@@ -29,14 +31,18 @@ exports.newMessage = async (req, res) => {
 //metodo para buscar el usuario por su correco electronico
 
 exports.getUserByEmail = async (req, res) => {
-  const { email } = req.body;
+  const { email } = req.query;
   try {
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findAll({ where: { 
+      email:{
+        [Op.like]: `%${email}%`
+      }
+     } });
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
     res.json({ user });
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching user profile' });
+    res.status(500).json({ error: 'Error fetching user profile', message: error.message });
   }
 };
