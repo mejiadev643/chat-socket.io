2c4724f745eb032443002b11a0ffd012187fd931
cristian mejia <comejia@innovacion.gob.sv>
2024-08-08 15:18:30 -0600

    agrupamiento de chats

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,4 +1,4 @@
-const { Message , User  } = require('../models');
+const { Message, User } = require('../models');
 const { Op } = require('sequelize'); // Importar operadores de Sequelize
 const jwt = require('jsonwebtoken');
 
@@ -16,10 +16,25 @@ exports.getChats = async (req, res) => {
                         { user_receive_id: userId }
                     ],
                 },
-                include:[{model: User, as: 'sended'}, {model: User, as: 'received'}]
+                include: [{ model: User, as: 'sended' }, { model: User, as: 'received' }]
             },
         );
-        res.json({ messages });
+        const groupedMessages = messages.reduce((acc, message) => {
+            const isSender = message.user_send_id === userId;
+            const otherUserId = isSender ? message.user_receive_id : message.user_send_id;
+            const otherUserName = isSender ? message.received.username : message.sended.username;
+            const OtherEmail = isSender ? message.received.email : message.sended.email;
+
+            if (!acc[otherUserId]) {
+                acc[otherUserId] = {
+                    user: { id: otherUserId, name: otherUserName, email: OtherEmail },
+                    messages: []
+                };
+            }
+            acc[otherUserId].messages.push(message);
+            return acc;
+        }, {});
+        res.json(Object.values(groupedMessages));
     } catch (error) {
         res.status(500).json({ error: 'Error fetching messages', message: error.message });
     }
